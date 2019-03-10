5609025aac66ff869d5569145970436cec0b1a99
Daniel Formel <dkfinmv@gmail.com>
2019-03-09 21:05:19 -0800

    Add response models

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 
+from typing import List
+
 from fastapi import FastAPI, Query, Path
 from models import TodoItem
 
@@ -10,16 +12,15 @@ with open('todos.json') as f:
     TODOS = json.load(f)
 
 
-@app.get('/todos')
+@app.get('/todos', response_model=List[TodoItem], summary='Get a list of todo items')
 async def get_todos(
     limit: int = Query(None, description='Limit the number of items returned', le=100)
 ):
     """Get a list of things to do"""
-    print(limit)
     return TODOS[0:limit]
 
 
-@app.get('/todos/{todo_id}')
+@app.get('/todos/{todo_id}', response_model=TodoItem, summary='Get a single todo item')
 async def get_todo(
     todo_id: int = Path(..., description='A todo item ID number to lookup')
 ):
@@ -27,7 +28,7 @@ async def get_todo(
     return TODOS[todo_id - 1]
 
 
-@app.post('/todos', status_code=201)
+@app.post('/todos', status_code=201, response_model=TodoItem, summary='Add a todo item')
 async def create_todo(todo: TodoItem):
     item = todo.dict()
     item['id'] = len(TODOS) + 1
